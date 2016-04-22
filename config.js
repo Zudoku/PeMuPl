c2226eae26e5af012470cf1946765405dfd721f9
PullaPoski <arttu.siren@gmail.com>
2016-04-22 20:08:17 +0300

    include config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+//WEB APP
+config.webserver_port = 3880;
+config.webserver_bind = '127.0.0.1';
+//DATABASE
+config.database_path = 'database';
+
+config.MODE = "DEV";
+
+config.password = ;
+
+
+module.exports = config;
